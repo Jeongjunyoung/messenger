c2148dba414a16ee6bb17ccef6624f8c520d51f5
JY <d1jung23@gmail.com>
2017-01-23 21:24:27 +0900

    first commit

---
+++ messenger/src/main/java/ms/mscom/msapp/HomeController.java
@@ -0,0 +1,16 @@
+package ms.mscom.msapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/chat")
+	public String home() {
+		return "chat";
+	}
+}
